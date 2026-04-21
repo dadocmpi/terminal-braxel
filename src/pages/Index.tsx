f23334e8047d700e822c8b1add4188bf482f89e1
[dyad] <bernardofx.pessoal@gmail.com>
2026-04-21 11:46:49 +0200

    [dyad] Terminal BRAXEL MARKETS finalizado e 100% operacional - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,30 +10,44 @@ import { MarketNews } from '../components/MarketNews';
 import { WorldClocks } from '../components/WorldClocks';
 import { PremiumDiscountGrid } from '../components/PremiumDiscountGrid';
 import { ZoneActivityFeed } from '../components/ZoneActivityFeed';
+import { SessionTimelineBar } from '../components/SessionTimelineBar';
+import { Watchlist } from '../components/Watchlist';
+import { InstitutionalBrain } from '../components/InstitutionalBrain';
 
 const LiveTerminal = () => {
   return (
     <div className="min-h-screen bg-background text-foreground flex flex-col">
       <DashboardHeader />
+      <SessionTimelineBar />
       
       <main className="flex-1 flex flex-col overflow-hidden">
         <StatusCards />
         <ActiveSignal />
         
         <div className="flex-1 grid grid-cols-12 gap-0 overflow-hidden">
-          {/* Coluna Esquerda: Clocks e Gráfico Principal (8 cols) */}
-          <div className="col-span-12 lg:col-span-8 border-r border-white/5 flex flex-col bg-black">
+          {/* Coluna Esquerda: Watchlist e Brain (3 cols) */}
+          <div className="hidden lg:flex col-span-3 border-r border-white/5 flex-col bg-black">
+            <div className="flex-1 border-b border-white/5">
+              <Watchlist />
+            </div>
+            <div className="p-4">
+              <InstitutionalBrain />
+            </div>
+          </div>
+
+          {/* Coluna Central: Clocks e Gráfico Principal (6 cols) */}
+          <div className="col-span-12 lg:col-span-6 border-r border-white/5 flex flex-col bg-black">
             <WorldClocks />
-            <div className="flex-[2] relative w-full">
+            <div className="flex-[2] relative w-full border-b border-white/5">
               <MainIndexChart />
             </div>
-            <div className="flex-1 border-t border-white/5 overflow-hidden">
+            <div className="flex-1 overflow-hidden">
               <MarketNews />
             </div>
           </div>
 
-          {/* Coluna Direita: P/D Grid e Feed (4 cols) */}
-          <div className="col-span-12 lg:col-span-4 flex flex-col bg-black">
+          {/* Coluna Direita: P/D Grid e Feed (3 cols) */}
+          <div className="col-span-12 lg:col-span-3 flex flex-col bg-black">
             <div className="flex-1 border-b border-white/5">
               <PremiumDiscountGrid />
             </div>
