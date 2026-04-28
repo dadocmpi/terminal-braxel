388b19e6d93cf7039e294ecd1eec75ad7fc64636
dadocmpi <bernardofx.pessoal@gmail.com>
2026-04-28 12:04:29 +0000

    Wrap application in TradingProvider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-﻿import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { TradingProvider } from "./contexts/TradingContext";
 import Access from "./pages/Access";
 import Analytics from "./pages/Analytics";
 import Index from "./pages/Index";
@@ -12,18 +13,20 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/access" element={<Access />} />
-          <Route path="/" element={<Index />} />
-          <Route path="/analytics" element={<Analytics />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <TradingProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/access" element={<Access />} />
+            <Route path="/" element={<Index />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </TradingProvider>
   </QueryClientProvider>
 );
 
